040cc2293fa5a0cb856e4a42ab35b9abc7bcd207
KamilcanCelik <kamilcancelik5757@gmail.com>
2021-05-07 12:52:07 +0300

    Update Category.tsx

--- Components/categories/Category.tsx
+++ Components/categories/Category.tsx
@@ -8,25 +8,25 @@ import styles from '../../styles/Category.module.css';
 export default function Category({}) {
   const { data, loading } = useQuery(GET_ALL_CATEGORIES);
 
-  if (loading) {
-    return (
-      <div
-        className={`border ${styles.category}`}
-        style={{ borderRadius: '0.5rem' }}
-      >
-        <div className="nav-scroller py-1 mb-2">
-          <nav className="nav d-flex justify-content-between">
-            <div className="text-decoration-none text-dark">
-              <div
-                style={{ cursor: 'pointer' }}
-                className="p-2 text-muted"
-              ></div>
-            </div>
-          </nav>
-        </div>
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div
+  //       className={`border ${styles.category}`}
+  //       style={{ borderRadius: '0.5rem' }}
+  //     >
+  //       <div className="nav-scroller py-1 mb-2">
+  //         <nav className="nav d-flex justify-content-between">
+  //           <div className="text-decoration-none text-dark">
+  //             <div
+  //               style={{ cursor: 'pointer' }}
+  //               className="p-2 text-muted"
+  //             ></div>
+  //           </div>
+  //         </nav>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div
