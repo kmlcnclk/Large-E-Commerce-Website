c7d1f6646b985aa823c276fdfa22d67f9f71e9e9
KamilcanCelik <kamilcancelik5757@gmail.com>
2021-05-07 12:06:41 +0300

    e-commerce

--- src/schema.js
+++ src/schema.js
@@ -2,4 +2,4 @@ import { resolvers } from 'GraphQL/Apollo-Server/Resolvers/resolvers';
 import { typeDefs } from 'GraphQL/Apollo-Server/TypeDefs/typeDefs';
 import { makeExecutableSchema } from 'apollo-server';
 
-export const schema = makeExecutableSchema({ resolvers, typeDefs });
+export const schema = makeExecutableSchema({ typeDefs, resolvers });
